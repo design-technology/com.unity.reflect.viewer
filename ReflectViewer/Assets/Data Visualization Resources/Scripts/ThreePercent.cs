41918745214d9425a67a2f6055d5e3928f96de41
dt-js <justyna.szychowska@grimshaw.global>
2021-07-07 13:13:39 +0100

    dashboard assets added for testing

--- ReflectViewer/Assets/Data Visualization Resources/Scripts/ThreePercent.cs
+++ ReflectViewer/Assets/Data Visualization Resources/Scripts/ThreePercent.cs
@@ -4,40 +4,42 @@ using UnityEngine.UI;
 
 public class ThreePercent : MonoBehaviour
 {
-    [Range(0,9999)]
-    public float no1, no2, no3;
 
-    public Image a, b, c;
-    public Text t1, t2, t3;
+    public float solidPanel, glassPanel;
+
+    public Image solidPanelCircle, glassPanelCircle;
+ 
+    public Text ratio1, ratio2;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        solidPanel = 0.75f;
+        glassPanel = 0.25f;
+        UpdatePercent(solidPanel, glassPanel);
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdatePercent(no1,no2,no3);
+        //UpdatePercent(no1,no2);
     }
-    public void UpdatePercent(float n1,float n2,float n3)
+    public void UpdatePercent(float n1,float n2)
     {
-        float sum = n1 + n2 + n3;
+        float sum = n1 + n2;
         float p1 = n1 / sum;
         float p2 = n2 / sum;
-        float p3 = n3 / sum;
-
-        a.fillAmount = p1;
-        b.fillAmount = p2;
-        b.transform.localEulerAngles = -new Vector3(0, 0, 360 * p1);
-
-        c.fillAmount = p3;
-        c.transform.localEulerAngles = -new Vector3(0, 0, 360 * p1) - new Vector3(0, 0, 360 * p2);
 
 
-        t1.text = p1 * 100 + "%";
-        t2.text = p2 * 100 + "%";
-        t3.text = p3 * 100 + "%";
+        solidPanelCircle.fillAmount = p1 - 0.02f;
+        glassPanelCircle.fillAmount = p2 - 0.02f;
+        glassPanelCircle.transform.localEulerAngles = -new Vector3(0, 0, 360 * p1);
+
+
+
+
+        ratio1.text = (int)(p1 * 100) + "%";
+        ratio2.text = (int)(p2 * 100) + "%";
+
     }
 }
--- ReflectViewer/Assets/Data Visualization Resources/Scripts/Wavecircle.cs
+++ ReflectViewer/Assets/Data Visualization Resources/Scripts/Wavecircle.cs
@@ -5,29 +5,93 @@ using UnityEngine.UI;
 
 public class Wavecircle : MonoBehaviour
 {
+    float max = 200;
+    float min = 40;
+
+    float currentValue;
+    public string dataName;
+
+    public Image image;
+    public Text progress;
+    public Text actualValue;
+    public Text title;
+
+    bool up = true;
+    int frameCount = 0;
+
+
     [Range(0, 100)]
     public float no1;
 
     public Transform wave;
     public Transform s, e;
 
-    public Text theText;
 
     void Start()
     {
-        
+        currentValue = min;
+        title.text = dataName;
     }
 
     // Update is called once per frame
     void Update()
     {
-        UpdatePercent(no1);
+        if (frameCount % 40 == 0)
+        {
+            UpdateValue();
+        }
+
+        frameCount++;
     }
 
-    void UpdatePercent(float f)
-    {
-        wave.position = s.position + (e.position - s.position) * f / 100;
+    //void UpdatePercent(float f)
+    //{
+    //    wave.position = s.position + (e.position - s.position) * f / 100;
 
-        theText.text = Mathf.RoundToInt(f) + "%";
+    //    theText.text = Mathf.RoundToInt(f) + "%";
+    //}
+
+    void AddOne()
+    {
+        currentValue++;
+    }
+
+    void MinusOne()
+    {
+        currentValue--;
+    }
+
+
+    void UpdateValue()
+    {
+        if (currentValue >= max)
+        {
+            up = false;
+        }
+        else if (currentValue <= min)
+        {
+            up = true;
+        }
+
+        if (up)
+        {
+            AddOne();
+        }
+        else
+        {
+            MinusOne();
+        }
+
+        float val = (currentValue - min) / (max - min)*1f;
+
+        wave.position = s.position + (e.position - s.position) * val;
+        actualValue.text = currentValue + " DF";
+        progress.text = Mathf.RoundToInt(val * 100) + " %";
+
+
+        //var perc = (int)(image.fillAmount * 100) + "%";
+        //progress.text = perc;
+
+        // actualValue.text = currentValue + " dd";
     }
 }
